5cc71814343242b2e2454137666f6a466c25196b
Erutuon <5840197+Erutuon@users.noreply.github.com>
2018-10-23 19:58:06 -0500

    more like a testcase

--- bin/lua/identifier.lua
+++ bin/lua/identifier.lua
@@ -1,5 +1,33 @@
-function 가 (...)
-	return ...
+local function assert_compile(chunk, should_compile)
+	local success, val = pcall(load, chunk)
+	
+	if success then
+		success = pcall(val)
+	end
+	
+	if success ~= should_compile then
+		local msg = (should_compile and "failed to compile" or "succeeded at compiling")
+			.. " chunk"
+		if not success then
+			msg = msg .. " with error '" .. val .. "'"
+		end
+		msg = msg .. ":\n" .. tostring(chunk)
+		print(msg)
+	end
 end
 
-print(가('Hello', 'world!'))
+-- Hangul syllable gag (U+D573) allowed in an identifier.
+assert_compile("\z
+function \u{D573} (...) \
+	return ... \
+end \
+\
+\u{D573}('Hello', 'world!')", true)
+
+-- No-break space (U+00A0) not allowed in an identifier.
+assert_compile("\z
+function \u{A0} (...) \
+	return ... \
+end \
+\
+\u{A0}('Hello', 'world!')", false)
